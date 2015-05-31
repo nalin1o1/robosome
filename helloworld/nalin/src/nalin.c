a335a61712929f7eafcf1f61ed1ddc58e8c5851c
Nalin Gabriel <nalin.gaby@gmail.com>
2015-05-31 17:35:29 +0530

    to learn programming

---
+++ helloworld/nalin/src/nalin.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : nalin.c
+ Author      : Nalin
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
---
+++ helloworld/nalinwork/src/nalinwork.c
@@ -0,0 +1,30 @@
+/*
+ ============================================================================
+][pl,,mlm knjnij90o Name        : nalinwork.c
+ Author      : Nalin
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	int a = 1;
+	int z = 5;
+	int b = 30;
+	int c =a + b;
+	printf("display the number %d", c);
+	for ( a =1 ; a < 10 ;  a= a+1 )
+	{
+		b = a * z;
+		printf("display the number %d \n", b);
+	}
+
+printf("display the number a %d  \n ", a );
+
+	return EXIT_SUCCESS;
+}
